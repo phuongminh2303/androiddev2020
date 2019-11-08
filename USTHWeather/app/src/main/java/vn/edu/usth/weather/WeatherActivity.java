e9ab7286083a6a10c8afe32ac527a565b01c6d8c
phuongminh2303 <phuongminh2303@gmail.com>
2019-11-08 15:21:29 +0700

    Labwork 1 and 2 source file commit

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,46 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("Info", "onCreate() method here!!!");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Info", "onStart() method here!!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Info", "onResume() method here!!!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Info", "onPause() method here!!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Info", "onStop() method here!!!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Info", "onDestroy() method here!!!");
+    }
+}
