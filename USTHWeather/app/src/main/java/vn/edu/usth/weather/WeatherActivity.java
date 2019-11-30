65b104fbe37b768244dd8033e7c4ad69cb4ff7eb
phuongminh2303 <phuongminh2303@gmail.com>
2019-12-01 00:24:09 +0700

    Finish labwork 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,14 +1,29 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
+    MediaPlayer music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,37 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        // Copy music file to sdcard0
+        copyFileToExternalStorage(R.raw.skygarden, "skygarden.mp3");
+
+        // Play music in the app
+        music = MediaPlayer.create(WeatherActivity.this, R.raw.skygarden);
+        music.start();
+
+    }
+
+    private void copyFileToExternalStorage(int resourceId, String resourceName){
+        String pathSDCard = Environment.getExternalStorageDirectory()
+                + "/Android/data/vn.edu.usth.weather/" + resourceName;
+        try{
+            InputStream in = getResources().openRawResource(resourceId);
+            FileOutputStream out = null;
+            out = new FileOutputStream(pathSDCard);
+            byte[] buff = new byte[1024];
+            int read = 0;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            } finally {
+                in.close();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -54,6 +100,8 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.i("Info", "onStop() method here!!!");
+        // Stop background music when close the program
+        music.stop();
     }
 
     @Override
